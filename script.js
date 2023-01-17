a93ca2df81fae01936bac60a4afa5c218c2ab9de
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2023-01-17 16:39:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,48 @@
-/* Edit this file */
-const player = document.querySelector('.player');
-const video = player.querySelector('.viewer');
-const progress = player.querySelector('.progress');
-const progressBar = player.querySelector('.progress__filled');
-const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelectorAll('.player__slider');
+const video = document.querySelector("video");
+const progressBar = document.querySelector(".progress__filled");
+const playPauseButton = document.querySelector(".player__button");
+const volumeInput = document.querySelector("input[name='volume']");
+const playbackSpeedInput = document.querySelector("input[name='playbackSpeed']");
+const rewindButton = document.querySelector(".rewind");
+const forwardButton = document.querySelector(".forward");
+
+let isPlaying = false;
+
+function togglePlayPause() {
+    if (isPlaying) {
+        video.pause();
+        playPauseButton.textContent = "►";
+    } else {
+        video.play();
+        playPauseButton.textContent = "❚ ❚";
+    }
+    isPlaying = !isPlaying;
+}
+
+function updateProgressBar() {
+    progressBar.style.flexBasis = (video.currentTime / video.duration) * 100 + "%";
+}
+
+function setVolume() {
+    video.volume = volumeInput.value;
+}
+
+function setPlaybackSpeed() {
+    video.playbackRate = playbackSpeedInput.value;
+}
+
+function rewind() {
+    video.currentTime -= 10;
+}
+
+function forward() {
+    video.currentTime += 25;
+}
+
+video.addEventListener("click", togglePlayPause);
+video.addEventListener("timeupdate", updateProgressBar);
+playPauseButton.addEventListener("click", togglePlayPause);
+volumeInput.addEventListener("input", setVolume);
+playbackSpeedInput.addEventListener("input", setPlaybackSpeed);
+rewindButton.addEventListener("click", rewind);
+forwardButton.addEventListener("click", forward);
